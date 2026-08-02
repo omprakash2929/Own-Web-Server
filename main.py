ced168c745ab6e7a2b0457d4a80e8184895798d9
omprakash2929 <om.prakashchauhan2929@gmail.com>
2026-08-02 13:41:12 +0530

    feat:socket setup for web server

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+server_socket.bind(("0.0.0.0",8080))
+
 print("Server are initialization")
