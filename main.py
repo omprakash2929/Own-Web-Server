0ff7cbde5990c72e6057c7fd7726e113437c8cde
omprakash2929 <om.prakashchauhan2929@gmail.com>
2026-08-04 18:36:20 +0530

    feat: accept client connections and parse HTTP headers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import socket
-
+import  time
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8080
 
@@ -11,4 +11,19 @@ server_socket.bind((SERVER_HOST,SERVER_PORT))
 server_socket.listen(5)
 
 print(f"Listening on port:{SERVER_PORT}...")
+try:
+    while True:
+            client_socket, client_address = server_socket.accept()
+            req = client_socket.recv(1500).decode()
+            print(req)
+            headers = req.split('\n')
+            
+            first_header_components = headers[0].split()
+            http_method = first_header_components[0]
+            path = first_header_components[1]
+            
+except KeyboardInterrupt:
+    print("\nServer stopped.")
 
+finally:
+    server_socket.close()     
