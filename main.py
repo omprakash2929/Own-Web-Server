8e5d8cbb4e8ac9a5e68847662d5020612ae3b1fa
omprakash2929 <om.prakashchauhan2929@gmail.com>
2026-08-04 17:43:11 +0530

    Created a basic server socket to listrning port

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import socket
 
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8080
+
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 
-server_socket.bind(("0.0.0.0",8080))
+server_socket.bind((SERVER_HOST,SERVER_PORT))
 
-print("Server are initialization")
+server_socket.listen(5)
+
+print(f"Listening on port:{SERVER_PORT}...")
+
