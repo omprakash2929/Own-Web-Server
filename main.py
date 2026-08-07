ac64af337428e66b3a704870ca8a8ac760aa4ef5
omprakash2929 <om.prakashchauhan2929@gmail.com>
2026-08-07 18:34:16 +0530

    feat: Completed handle client funcation

--- main.py
+++ main.py
@@ -5,7 +5,37 @@ SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 2300
 
 def handle_client(client_socket, client_address):
-    ...
+    try:
+        req = client_socket.recv(4096).decode(errors="ignore")
+        if not  req:
+            return
+        request_line = req.split('\r\n')[0]
+        method, path, _ = request_line.split()
+        
+        if method == "GET" and path == "/":
+            with open("index.html", "rb") as f:
+                content = f.read()
+            response = (
+                b"HTTP/1.1 200 ok\r\n"
+                b"Content-Type: text/html; charset=utf-8\r\n"
+                + f"Content-Lenght: {len(content)}\r\n".encode()
+                + b"Connection: close\r\n\r\n"
+                + content
+            )
+        else:
+            body = b"404 Not Found"
+            response = (
+                b"HTTP/1.1 404 Not Found\r\n"
+                b"Content-Type: text/plain\r\n"
+                + f"Content-Length: {len(body)}\r\n".encode()
+                + b"\r\n" + body
+            )
+            
+        client_socket.sendall(response)
+    except Exception as e:
+        print("Error:",e)
+    finally:
+        client_socket.close()
 
 def main():
     server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
